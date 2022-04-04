8f7ce99a17d3468ce6aac90b2e78b5ff471f9c5d
Yunus Kazimov <yunus.kazimov@gmail.com>
2022-04-04 05:42:04 +0400

    task2: add unit tests for Transfer Service

--- src/main/java/az/unibank/unitechmstransfer/controller/error/ErrorHandler.java
+++ src/main/java/az/unibank/unitechmstransfer/controller/error/ErrorHandler.java
@@ -1,9 +1,11 @@
 package az.unibank.unitechmstransfer.controller.error;
 
 import az.unibank.unitechmstransfer.model.ErrorDto;
+import az.unibank.unitechmstransfer.model.exception.AccountNotFoundException;
 import az.unibank.unitechmstransfer.model.exception.NoEnoughMoneyException;
 import az.unibank.unitechmstransfer.model.exception.NonActiveAccountException;
 import az.unibank.unitechmstransfer.model.exception.SameAccountException;
+import az.unibank.unitechmstransfer.model.exception.UnsupportedTransferMoneyException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
@@ -14,7 +16,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.*;
+import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.MONEY_IS_ZERO_OR_NEGATIVE;
+import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.NON_ACTIVE;
+import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.NOT_FOUND;
+import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.NO_MONEY;
+import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.SAME_ACCOUNT;
+import static az.unibank.unitechmstransfer.model.exception.ErrorCodes.UNEXPECTED_EXCEPTION;
 
 @ControllerAdvice
 public class ErrorHandler extends ResponseEntityExceptionHandler {
@@ -30,6 +37,18 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
                         .message(ex.getMessage())
                         .build(),
                 new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
+    }
+
+    @ExceptionHandler(AccountNotFoundException.class)
+    public ResponseEntity<Object> handleAccountNotFoundException(AccountNotFoundException ex,
+                                                                 WebRequest webRequest) {
+        logger.info(ex.toString());
+
+        return handleExceptionInternal(ex, ErrorDto.builder()
+                        .code(NOT_FOUND)
+                        .message(ex.getMessage())
+                        .build(),
+                new HttpHeaders(), HttpStatus.NOT_FOUND, webRequest);
     }
 
     @ExceptionHandler(NoEnoughMoneyException.class)
@@ -51,6 +70,18 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 
         return handleExceptionInternal(ex, ErrorDto.builder()
                         .code(NON_ACTIVE)
+                        .message(ex.getMessage())
+                        .build(),
+                new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
+    }
+
+    @ExceptionHandler(UnsupportedTransferMoneyException.class)
+    public ResponseEntity<Object> handleUnsupportedTransferMoneyException(
+            UnsupportedTransferMoneyException ex, WebRequest webRequest) {
+        logger.info(ex.toString());
+
+        return handleExceptionInternal(ex, ErrorDto.builder()
+                        .code(MONEY_IS_ZERO_OR_NEGATIVE)
                         .message(ex.getMessage())
                         .build(),
                 new HttpHeaders(), HttpStatus.CONFLICT, webRequest);
--- src/main/java/az/unibank/unitechmstransfer/model/exception/AccountNotFoundException.java
+++ src/main/java/az/unibank/unitechmstransfer/model/exception/AccountNotFoundException.java
@@ -3,24 +3,24 @@ package az.unibank.unitechmstransfer.model.exception;
 import lombok.Getter;
 
 @Getter
-public class SameAccountException extends RuntimeException {
+public class AccountNotFoundException extends RuntimeException {
     private final String code;
     private final String message;
 
-    public SameAccountException(String code, String message) {
+    public AccountNotFoundException(String code, String message) {
         super(message);
         this.code = code;
         this.message = message;
     }
 
-    public SameAccountException(String message) {
+    public AccountNotFoundException(String message) {
         super(message);
         this.code = null;
         this.message = message;
     }
 
-    public static SameAccountException of(String errorCode, String errorMessage) {
-        return new SameAccountException(errorCode, errorMessage);
+    public static AccountNotFoundException of(String errorCode, String errorMessage) {
+        return new AccountNotFoundException(errorCode, errorMessage);
     }
 
     @Override
